f1d81bc01ea258dbf3e5bf88b71792c588176563
thisLucio <luxusnietzsche@gmail.com>
2024-09-17 23:15:16 -0300

    Setup Spring Project

---
+++ src/test/java/br/com/thislucio/tutorial_spring_data_pagination/TutorialSpringDataPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thislucio.tutorial_spring_data_pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringDataPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
